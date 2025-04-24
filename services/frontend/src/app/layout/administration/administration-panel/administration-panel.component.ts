094efd9cd243bac2624088479412b700c79a4a60
TUSHAR <tushar.tushar2@soprasteria.com>
2025-04-24 12:59:36 +0530

    feat(885): hidden button functionality

--- services/frontend/src/app/layout/administration/administration-panel/administration-panel.component.ts
+++ services/frontend/src/app/layout/administration/administration-panel/administration-panel.component.ts
@@ -5,7 +5,7 @@
  * This product includes software developed by
  * French Ecological Ministery (https://gitlab-forge.din.developpement-durable.gouv.fr/pub/numeco/m4g/numecoeval)
  */
-import { Component, inject } from "@angular/core";
+import { Component, ElementRef, inject, ViewChild } from "@angular/core";
 import { Router } from "@angular/router";
 import { TranslateService } from "@ngx-translate/core";
 import { MenuItem } from "primeng/api";
@@ -20,7 +20,7 @@ export class AdministrationPanelComponent {
     public globalStore = inject(GlobalStoreService);
 
     tabMenuList!: MenuItem[];
-
+    @ViewChild("mainContent") mainContent!: ElementRef;
     constructor(
         public userService: UserService,
         public router: Router,
@@ -55,5 +55,16 @@ export class AdministrationPanelComponent {
                 });
             }
         });
+    }
+
+    focusFirstElement() {
+        const mainElement = this.mainContent.nativeElement;
+        const focusableElements = mainElement.querySelectorAll(
+            'a, button, input, textarea, select, [tabindex]:not([tabindex="-1"])',
+        );
+
+        if (focusableElements.length > 0) {
+            (focusableElements[0] as HTMLElement).focus();
+        }
     }
 }
--- services/frontend/src/app/layout/layout.component.ts
+++ services/frontend/src/app/layout/layout.component.ts
@@ -5,7 +5,7 @@
  * This product includes software developed by
  * French Ecological Ministery (https://gitlab-forge.din.developpement-durable.gouv.fr/pub/numeco/m4g/numecoeval)
  */
-import { Component, inject } from "@angular/core";
+import { Component, ElementRef, inject, ViewChild } from "@angular/core";
 import { GlobalStoreService } from "../core/store/global.store";
 
 @Component({
@@ -14,4 +14,17 @@ import { GlobalStoreService } from "../core/store/global.store";
 })
 export class LayoutComponent {
     public globalStore = inject(GlobalStoreService);
+
+    @ViewChild("mainContent") mainContent!: ElementRef;
+
+    focusFirstElement() {
+        const mainElement = this.mainContent.nativeElement;
+        const focusableElements = mainElement.querySelectorAll(
+            'a, button, input, textarea, select, [tabindex]:not([tabindex="-1"])',
+        );
+
+        if (focusableElements.length > 0) {
+            (focusableElements[0] as HTMLElement).focus();
+        }
+    }
 }
