d5ab1922f2deff288ddf5b942803357446b35c84
TUSHAR <tushar.tushar2@soprasteria.com>
2025-05-30 14:07:37 +0530

    feat(1049): test fix

--- services/frontend/src/app/layout/about-us/declarations/declarations.component.spec.ts
+++ services/frontend/src/app/layout/about-us/declarations/declarations.component.spec.ts
@@ -1,23 +1,32 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from "@angular/core/testing";
 
-import { DeclarationsComponent } from './declarations.component';
+import { ActivatedRoute } from "@angular/router";
+import { TranslateModule } from "@ngx-translate/core";
+import { DeclarationsComponent } from "./declarations.component";
 
-describe('DeclarationsComponent', () => {
-  let component: DeclarationsComponent;
-  let fixture: ComponentFixture<DeclarationsComponent>;
+describe("DeclarationsComponent", () => {
+    let component: DeclarationsComponent;
+    let fixture: ComponentFixture<DeclarationsComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [DeclarationsComponent]
-    })
-    .compileComponents();
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            imports: [TranslateModule.forRoot(), DeclarationsComponent],
+            providers: [
+                {
+                    provide: ActivatedRoute,
+                    useValue: {
+                        snapshot: { data: {} },
+                    },
+                },
+            ],
+        }).compileComponents();
 
-    fixture = TestBed.createComponent(DeclarationsComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+        fixture = TestBed.createComponent(DeclarationsComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it("should create", () => {
+        expect(component).toBeTruthy();
+    });
 });
