7e702c7d416acdfc7b9472ca597c4bbb5d486a8b
TUSHAR <tushar.tushar2@soprasteria.com>
2025-05-30 17:03:29 +0530

    feat(1149): fix test

--- services/frontend/src/app/layout/about-us/declarations/declarations.component.spec.ts
+++ services/frontend/src/app/layout/about-us/declarations/declarations.component.spec.ts
@@ -1,7 +1,9 @@
 import { ComponentFixture, TestBed } from "@angular/core/testing";
 
+import { provideHttpClient, withInterceptorsFromDi } from "@angular/common/http";
 import { ActivatedRoute } from "@angular/router";
 import { TranslateModule } from "@ngx-translate/core";
+import { MessageService } from "primeng/api";
 import { DeclarationsComponent } from "./declarations.component";
 
 describe("DeclarationsComponent", () => {
@@ -12,11 +14,16 @@ describe("DeclarationsComponent", () => {
         await TestBed.configureTestingModule({
             imports: [TranslateModule.forRoot(), DeclarationsComponent],
             providers: [
+                provideHttpClient(withInterceptorsFromDi()),
                 {
                     provide: ActivatedRoute,
                     useValue: {
                         snapshot: { data: {} },
                     },
+                },
+                {
+                    provide: MessageService,
+                    useValue: {}, // Mock implementation of MessageService
                 },
             ],
         }).compileComponents();
