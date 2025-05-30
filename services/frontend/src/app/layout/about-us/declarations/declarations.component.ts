177a821cfcf5133b157065e078bfdb8c7955ad9d
TUSHAR <tushar.tushar2@soprasteria.com>
2025-05-30 16:54:26 +0530

    feat(1149): fix mailto

--- services/frontend/src/app/layout/about-us/declarations/declarations.component.ts
+++ services/frontend/src/app/layout/about-us/declarations/declarations.component.ts
@@ -1,7 +1,11 @@
-import { Component, inject } from "@angular/core";
+import { Component, DestroyRef, inject } from "@angular/core";
+import { takeUntilDestroyed } from "@angular/core/rxjs-interop";
 import { Title } from "@angular/platform-browser";
 import { TranslateService } from "@ngx-translate/core";
+import { Organization, Subscriber } from "src/app/core/interfaces/user.interfaces";
+import { UserService } from "src/app/core/service/business/user.service";
 import { SharedModule } from "src/app/core/shared/shared.module";
+import { Constants } from "src/constants";
 
 @Component({
     selector: "app-declarations",
@@ -13,7 +17,11 @@ import { SharedModule } from "src/app/core/shared/shared.module";
 export class DeclarationsComponent {
     private readonly translate = inject(TranslateService);
     private readonly titleService = inject(Title);
+    private readonly userService = inject(UserService);
+    private readonly destroyRef = inject(DestroyRef);
     currentLang: string = this.translate.currentLang;
+    currentSubscriber: Subscriber = {} as Subscriber;
+    selectedOrganization: Organization = {} as Organization;
     pdfSize = 0;
     ngOnInit() {
         this.translate.get("declarations.title").subscribe((translatedTitle: string) => {
@@ -21,5 +29,21 @@ export class DeclarationsComponent {
         });
         this.currentLang = this.translate.currentLang;
         this.pdfSize = this.currentLang === "en" ? 139 : 253;
+
+        this.userService.currentSubscriber$
+            .pipe(takeUntilDestroyed(this.destroyRef))
+            .subscribe((subscriber) => (this.currentSubscriber = subscriber));
+
+        this.userService.currentOrganization$
+            .pipe(takeUntilDestroyed(this.destroyRef))
+            .subscribe((organization: Organization) => {
+                this.selectedOrganization = organization;
+            });
+    }
+
+    composeEmail() {
+        let subject = `[${this.currentSubscriber.name}/${this.selectedOrganization?.id}] ${Constants.SUBJECT_MAIL}`;
+        let email = `mailto:${Constants.RECIPIENT_MAIL}?subject=${subject}`;
+        window.location.href = email;
     }
 }
