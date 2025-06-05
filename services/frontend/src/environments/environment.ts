fdcc15db5b981793e1d2dfa218e87a6f0523a462
sbouaziz <sal.bouaziz@gmail.com>
2025-06-05 14:33:42 +0200

    fix: error

--- services/frontend/src/environments/environment.ts
+++ services/frontend/src/environments/environment.ts
@@ -20,7 +20,7 @@ export const environment = {
         enabled: "true",
     },
     frontEndUrl: "http://localhost:4200",
-    subpath: "toto",
+    subpath: "",
     showBetaFeatures: "true",
     matomo: {
         matomoTagManager: {
