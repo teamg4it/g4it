0a5fe468b82443dfa89be01b19c74f6a12cf9456
TUSHAR <tushar.tushar2@soprasteria.com>
2025-07-10 16:36:49 +0530

    feat(1320): tests fix

---
+++ services/frontend/src/app/layout/header/header-siderbar/left-sidebar/left-sidebar.component.spec.ts
@@ -0,0 +1,36 @@
+import { TestBed } from "@angular/core/testing";
+import { Router } from "@angular/router";
+import { TranslateService } from "@ngx-translate/core";
+import { UserService } from "src/app/core/service/business/user.service";
+import { GlobalStoreService } from "src/app/core/store/global.store";
+import { LeftSidebarComponent } from "./left-sidebar.component";
+
+describe("LeftSidebarComponent", () => {
+    let component: LeftSidebarComponent;
+    let userServiceMock: jasmine.SpyObj<UserService>;
+
+    beforeEach(() => {
+        userServiceMock = jasmine.createSpyObj("UserService", ["getSelectedPage"]);
+
+        TestBed.configureTestingModule({
+            providers: [
+                LeftSidebarComponent,
+                { provide: UserService, useValue: userServiceMock },
+                { provide: GlobalStoreService, useValue: {} },
+                { provide: TranslateService, useValue: {} },
+                { provide: Router, useValue: {} },
+            ],
+        });
+
+        component = TestBed.inject(LeftSidebarComponent);
+    });
+
+    it("should set the selected page correctly", () => {
+        const mockSelectedPage = "mock-page";
+        userServiceMock.getSelectedPage.and.returnValue(mockSelectedPage);
+
+        component.setSelectedPage();
+
+        expect(component.selectedPage()).toBe(mockSelectedPage);
+    });
+});
