8354d15e2af9889beca5a631b73f864f29cc3b79
TUSHAR <tushar.tushar2@soprasteria.com>
2025-08-02 17:52:16 +0530

    feat(1349): test fix

--- services/frontend/src/app/layout/digital-services-footprint/digital-services-resources/digital-services-resources.component.spec.ts
+++ services/frontend/src/app/layout/digital-services-footprint/digital-services-resources/digital-services-resources.component.spec.ts
@@ -1,23 +1,22 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from "@angular/core/testing";
 
-import { DigitalServicesResourcesComponent } from './digital-services-resources.component';
+import { DigitalServicesResourcesComponent } from "./digital-services-resources.component";
 
-describe('DigitalServicesResourcesComponent', () => {
-  let component: DigitalServicesResourcesComponent;
-  let fixture: ComponentFixture<DigitalServicesResourcesComponent>;
+describe("DigitalServicesResourcesComponent", () => {
+    let component: DigitalServicesResourcesComponent;
+    let fixture: ComponentFixture<DigitalServicesResourcesComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [DigitalServicesResourcesComponent]
-    })
-    .compileComponents();
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [DigitalServicesResourcesComponent],
+        }).compileComponents();
 
-    fixture = TestBed.createComponent(DigitalServicesResourcesComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+        fixture = TestBed.createComponent(DigitalServicesResourcesComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it("should create", () => {
+        expect(component).toBeTruthy();
+    });
 });
