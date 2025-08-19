8c1e3a5b7b4844d93d8c0c8f8c5580a5ed8c7bf2
Diksha <diksha.takuli@soprasteria.com>
2025-08-19 16:33:49 +0530

    feat(1103): update inventoryService test coverage

--- services/backend/src/test/java/com/soprasteria/g4it/backend/apiinventory/business/InventoryServiceTest.java
+++ services/backend/src/test/java/com/soprasteria/g4it/backend/apiinventory/business/InventoryServiceTest.java
@@ -17,6 +17,9 @@ import com.soprasteria.g4it.backend.apiuser.business.OrganizationService;
 import com.soprasteria.g4it.backend.apiuser.model.UserBO;
 import com.soprasteria.g4it.backend.apiuser.modeldb.Organization;
 import com.soprasteria.g4it.backend.common.dbmodel.Note;
+import com.soprasteria.g4it.backend.common.task.modeldb.Task;
+import com.soprasteria.g4it.backend.common.task.repository.TaskRepository;
+import com.soprasteria.g4it.backend.exception.G4itRestException;
 import com.soprasteria.g4it.backend.server.gen.api.dto.InventoryCreateRest;
 import com.soprasteria.g4it.backend.server.gen.api.dto.InventoryType;
 import com.soprasteria.g4it.backend.server.gen.api.dto.InventoryUpdateRest;
@@ -31,9 +34,12 @@ import org.springframework.test.util.ReflectionTestUtils;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -41,6 +47,7 @@ class InventoryServiceTest {
 
     private static final String SUBSCRIBER = "SUBSCRIBER";
     private static final Long ORGANIZATION_ID = 1L;
+    private static final Long INVENTORY_ID = 2L;
 
     @InjectMocks
     private InventoryService inventoryService;
@@ -50,10 +57,62 @@ class InventoryServiceTest {
 
     @Mock
     private InventoryRepository inventoryRepo;
+    @Mock
+    private TaskRepository taskRepo;
 
     @BeforeEach
     void init() {
         ReflectionTestUtils.setField(inventoryService, "inventoryMapper", new InventoryMapperImpl());
+    }
+
+    @Test
+    void testGetCriteriaNumberReturnsSize() {
+        Long taskId = 1L;
+
+        Task task = Task.builder().id(taskId).criteria(List.of("Criteria1", "Criteria2")).build();
+
+        when(taskRepo.findById(taskId)).thenReturn(Optional.of(task));
+        Long result = inventoryService.getCriteriaNumber(taskId);
+
+        assertThat(result).isEqualTo(2L);
+        verify(taskRepo).findById(taskId);
+    }
+
+    @Test
+    void testGetCriteriaNumberTaskNotFoundThenThrow() {
+        Long taskId = 2L;
+
+        when(taskRepo.findById(taskId)).thenReturn(Optional.empty());
+        assertThrows(NoSuchElementException.class, () -> inventoryService.getCriteriaNumber(taskId));
+    }
+
+    @Test
+    void testInventoryExists() {
+
+        final Organization linkedOrganization = TestUtils.createOrganization();
+
+        when(organizationService.getOrganizationById(ORGANIZATION_ID)).thenReturn(linkedOrganization);
+        when(inventoryRepo.findByOrganizationAndId(linkedOrganization, INVENTORY_ID))
+                .thenReturn(Optional.of(new Inventory()));
+
+        boolean result = inventoryService.inventoryExists(SUBSCRIBER, ORGANIZATION_ID, INVENTORY_ID);
+
+        assertThat(result).isTrue();
+        verify(organizationService).getOrganizationById(ORGANIZATION_ID);
+        verify(inventoryRepo).findByOrganizationAndId(linkedOrganization, INVENTORY_ID);
+    }
+
+    @Test
+    void testInventoryDoesNotExist() {
+        final Organization linkedOrganization = TestUtils.createOrganization();
+
+        when(organizationService.getOrganizationById(ORGANIZATION_ID)).thenReturn(linkedOrganization);
+        when(inventoryRepo.findByOrganizationAndId(linkedOrganization, INVENTORY_ID)).thenReturn(Optional.empty());
+
+        boolean result = inventoryService.inventoryExists(SUBSCRIBER, ORGANIZATION_ID, INVENTORY_ID);
+        assertThat(result).isFalse();
+        verify(organizationService).getOrganizationById(ORGANIZATION_ID);
+        verify(inventoryRepo).findByOrganizationAndId(linkedOrganization, INVENTORY_ID);
     }
 
     @Test
@@ -83,7 +142,6 @@ class InventoryServiceTest {
     @Test
     void canRetrieveInventoriesFilteredByInventoryId() {
         final Organization linkedOrganization = TestUtils.createOrganization();
-        final Long inventoryId = 2L;
 
         final InventoryBO inventory1 = InventoryBO.builder().build();
         final List<InventoryBO> expectedInventoryList = List.of(inventory1);
@@ -92,25 +150,22 @@ class InventoryServiceTest {
         var inventoryOptional = Optional.of(inventoryEntity1);
 
         when(organizationService.getOrganizationById(ORGANIZATION_ID)).thenReturn(linkedOrganization);
-        when(this.inventoryRepo.findByOrganizationAndId(linkedOrganization, inventoryId)).thenReturn(inventoryOptional);
+        when(this.inventoryRepo.findByOrganizationAndId(linkedOrganization, INVENTORY_ID)).thenReturn(inventoryOptional);
 
-        final List<InventoryBO> result = this.inventoryService.getInventories(SUBSCRIBER, ORGANIZATION_ID, inventoryId);
+        final List<InventoryBO> result = this.inventoryService.getInventories(SUBSCRIBER, ORGANIZATION_ID, INVENTORY_ID);
 
         assertThat(result).hasSameSizeAs(expectedInventoryList);
 
         verify(organizationService, times(1)).getOrganizationById(ORGANIZATION_ID);
-        verify(inventoryRepo, times(1)).findByOrganizationAndId(linkedOrganization, inventoryId);
+        verify(inventoryRepo, times(1)).findByOrganizationAndId(linkedOrganization, INVENTORY_ID);
     }
 
     @Test
     void canRetrieveOneInventory() {
 
-        final Long inventoryId = 2L;
-
         final Inventory inventory = Inventory.builder()
                 .id(1L)
                 .build();
-
         final InventoryBO expected = InventoryBO.builder()
                 .id(1L)
                 .dataCenterCount(0L)
@@ -120,13 +175,30 @@ class InventoryServiceTest {
                 .tasks(List.of())
                 .build();
 
-        when(inventoryRepo.findByOrganizationAndId(any(), eq(inventoryId))).thenReturn(Optional.of(inventory));
+        when(inventoryRepo.findByOrganizationAndId(any(), eq(INVENTORY_ID))).thenReturn(Optional.of(inventory));
 
-        final InventoryBO result = inventoryService.getInventory(SUBSCRIBER, ORGANIZATION_ID, inventoryId);
+        final InventoryBO result = inventoryService.getInventory(SUBSCRIBER, ORGANIZATION_ID, INVENTORY_ID);
 
         assertThat(result).isEqualTo(expected);
 
-        verify(inventoryRepo, times(1)).findByOrganizationAndId(any(), eq(inventoryId));
+        verify(inventoryRepo, times(1)).findByOrganizationAndId(any(), eq(INVENTORY_ID));
+    }
+
+    @Test
+    void testGetInventoryThrowsExceptionWhenInventoryNotFound() {
+
+        final Organization linkedOrganization = TestUtils.createOrganization();
+        when(organizationService.getOrganizationById(ORGANIZATION_ID)).thenReturn(linkedOrganization);
+        when(inventoryRepo.findByOrganizationAndId(linkedOrganization, INVENTORY_ID)).thenReturn(Optional.empty());
+
+        G4itRestException exception = assertThrows(G4itRestException.class, () ->
+                inventoryService.getInventory(SUBSCRIBER, ORGANIZATION_ID, INVENTORY_ID)
+        );
+        assertThat(exception.getCode()).isEqualTo("404");
+        assertThat(exception.getMessage()).isEqualTo(String.format("inventory %d not found in %s/%s",
+                        INVENTORY_ID, SUBSCRIBER, ORGANIZATION_ID));
+        verify(organizationService).getOrganizationById(ORGANIZATION_ID);
+        verify(inventoryRepo).findByOrganizationAndId(linkedOrganization, INVENTORY_ID);
     }
 
 
@@ -162,7 +234,35 @@ class InventoryServiceTest {
     }
 
     @Test
-    void shouldUpdateInventory_UpdateCriteria() {
+    void shouldThrowWhenInventoryAlreadyExists() {
+        final Organization linkedOrganization = TestUtils.createOrganization();
+        final String inventoryName = "existingInventory";
+        final InventoryCreateRest inventoryCreateRest = InventoryCreateRest.builder()
+                .name(inventoryName)
+                .type(InventoryType.SIMULATION)
+                .build();
+
+        final UserBO userBo = TestUtils.createUserBONoRole();
+
+        when(organizationService.getOrganizationById(ORGANIZATION_ID)).thenReturn(linkedOrganization);
+        when(inventoryRepo.findByOrganizationAndName(linkedOrganization, inventoryName))
+                .thenReturn(Optional.of(new Inventory()));
+
+        G4itRestException exception = assertThrows(G4itRestException.class, () ->
+                inventoryService.createInventory(SUBSCRIBER, ORGANIZATION_ID, inventoryCreateRest, userBo)
+        );
+        assertThat(exception.getCode()).isEqualTo("409");
+        assertThat(
+                String.format("inventory %s already exists in %s/%s", inventoryName, SUBSCRIBER, ORGANIZATION_ID))
+                .isEqualTo(exception.getMessage());
+
+        verify(organizationService, times(1)).getOrganizationById(ORGANIZATION_ID);
+        verify(inventoryRepo, times(1)).findByOrganizationAndName(linkedOrganization, inventoryName);
+        verify(inventoryRepo, never()).save(any());
+    }
+
+    @Test
+    void shouldUpdateInventoryUpdateCriteria() {
         Long organizationId = 1L;
         final Organization linkedOrganization = TestUtils.createOrganization();
         UserBO userBo = TestUtils.createUserBONoRole();
@@ -190,7 +290,36 @@ class InventoryServiceTest {
     }
 
     @Test
-    void shouldUpdateInventory_UpdateNote() {
+    void shouldUpdateInventoryCreateNote() {
+        Long organizationId = 1L;
+        final Organization linkedOrganization = TestUtils.createOrganization();
+        UserBO userBo = TestUtils.createUserBONoRole();
+        final String inventoryName = "03-2023";
+        String subscriberName = "SUBSCRIBER";
+
+        final InventoryUpdateRest inventoryUpdateRest = InventoryUpdateRest.builder()
+                .id(1L)
+                .name(inventoryName)
+                .note(NoteUpsertRest.builder().content("newNote").build())
+                .build();
+        final Inventory inventory = Inventory
+                .builder()
+                .id(1L)
+                .organization(linkedOrganization).build();
+
+
+        when(organizationService.getOrganizationById(ORGANIZATION_ID)).thenReturn(linkedOrganization);
+        when(inventoryRepo.findByOrganizationAndId(linkedOrganization, 1L)).thenReturn(Optional.of(inventory));
+
+        InventoryBO result = inventoryService.updateInventory(subscriberName, organizationId, inventoryUpdateRest, userBo);
+
+        verify(inventoryRepo, times(1)).save(any());
+        assertThat(result.getNote().getContent()).isEqualTo("newNote");
+
+    }
+
+    @Test
+    void shouldUpdateInventoryUpdateNote() {
         Long organizationId = 1L;
         final Organization linkedOrganization = TestUtils.createOrganization();
         UserBO userBo = TestUtils.createUserBONoRole();
@@ -208,7 +337,6 @@ class InventoryServiceTest {
                 .note(Note.builder().content("note").build())
                 .organization(linkedOrganization).build();
 
-
         when(organizationService.getOrganizationById(ORGANIZATION_ID)).thenReturn(linkedOrganization);
         when(inventoryRepo.findByOrganizationAndId(linkedOrganization, 1L)).thenReturn(Optional.of(inventory));
 
@@ -216,6 +344,27 @@ class InventoryServiceTest {
 
         verify(inventoryRepo, times(1)).save(any());
         assertThat(result.getNote().getContent()).isEqualTo("newNote");
+
+    }
+    @Test
+    void UpdateInventoryNotFoundThrow() {
+        UserBO userBo = TestUtils.createUserBONoRole();
+        final InventoryUpdateRest inventoryUpdateRest = InventoryUpdateRest.builder()
+                .id(INVENTORY_ID)
+                .name("03-2023")
+                .build();
+            final Organization linkedOrganization = TestUtils.createOrganization();
+            when(organizationService.getOrganizationById(ORGANIZATION_ID)).thenReturn(linkedOrganization);
+            when(inventoryRepo.findByOrganizationAndId(linkedOrganization, INVENTORY_ID)).thenReturn(Optional.empty());
+
+            G4itRestException exception = assertThrows(G4itRestException.class, () ->
+                    inventoryService.updateInventory(SUBSCRIBER, ORGANIZATION_ID, inventoryUpdateRest, userBo)
+            );
+            assertThat(exception.getCode()).isEqualTo("404");
+            assertThat(exception.getMessage()).isEqualTo(String.format("inventory %d not found in %s/%s",
+                    INVENTORY_ID, SUBSCRIBER, ORGANIZATION_ID));
+            verify(organizationService).getOrganizationById(ORGANIZATION_ID);
+            verify(inventoryRepo).findByOrganizationAndId(linkedOrganization, INVENTORY_ID);
 
     }
 
