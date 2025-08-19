d8bb6c0a76ec120d0d21539f692ce23ec5be2ae3
Diksha <diksha.takuli@soprasteria.com>
2025-08-19 16:54:18 +0530

    feat(1453): update IS test case

--- services/backend/src/test/java/com/soprasteria/g4it/backend/apiinventory/business/InventoryServiceTest.java
+++ services/backend/src/test/java/com/soprasteria/g4it/backend/apiinventory/business/InventoryServiceTest.java
@@ -38,7 +38,6 @@ import java.util.NoSuchElementException;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
 
