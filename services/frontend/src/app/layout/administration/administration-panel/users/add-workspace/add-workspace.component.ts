b6101c63a7e1b651bc8b7da3561d4b6e451d30f6
TUSHAR <tushar.tushar2@soprasteria.com>
2025-09-09 16:17:06 +0530

    feat(1442): fix me api issue

--- services/frontend/src/app/layout/administration/administration-panel/users/add-workspace/add-workspace.component.ts
+++ services/frontend/src/app/layout/administration/administration-panel/users/add-workspace/add-workspace.component.ts
@@ -19,7 +19,7 @@ import { takeUntilDestroyed } from "@angular/core/rxjs-interop";
 import { Router } from "@angular/router";
 import { TranslateService } from "@ngx-translate/core";
 import { ConfirmationService, MessageService } from "primeng/api";
-import { take, switchMap, map } from "rxjs";
+import { take } from "rxjs";
 import { WorkspaceWithOrganization } from "src/app/core/interfaces/administration.interfaces";
 import { Role, RoleValue } from "src/app/core/interfaces/roles.interfaces";
 import { UserDetails } from "src/app/core/interfaces/user.interfaces";
@@ -168,31 +168,30 @@ export class AddWorkspaceComponent implements OnInit, OnChanges {
     }
 
     addUpdateWorkspace() {
-        const requestBody = this.getWorkspaceBody();
         this.administrationService
-            .postUserToWorkspaceAndAddRoles(requestBody)
-            .pipe(
-                switchMap(() => this.userService.user$),
-                takeUntilDestroyed(this.destroyRef),
-                switchMap(user =>
-                    this.userDataService.fetchUserInfo().pipe(
-                        take(1),
-                        map(() => user),
-                    ),
-                ),
-            )
-            .subscribe(user => {
-                const currentUserRoles = this.getWorkspaceBody().users.find(
-                    (u) => u.userId === user.id,
-                )?.roles;
-                const isAdmin =
-                    currentUserRoles?.includes(Role.OrganizationAdmin) ||
-                    currentUserRoles?.includes(Role.WorkspaceAdmin);
-                if (!isAdmin && currentUserRoles) {
-                    this.router.navigateByUrl(Constants.WELCOME_PAGE);
-                    return;
-                }
-                this.outClose.emit(false);
+            .postUserToWorkspaceAndAddRoles(this.getWorkspaceBody())
+            .subscribe(() => {
+                this.userService.user$
+                    .pipe(takeUntilDestroyed(this.destroyRef))
+                    .subscribe((user) => {
+                        this.userDataService
+                            .fetchUserInfo()
+                            .pipe(take(1))
+                            .subscribe(() => {
+                                const currentUserRoles =
+                                    this.getWorkspaceBody().users.find(
+                                        (u) => u.userId === user.id,
+                                    )?.roles;
+                                const isAdmin =
+                                    currentUserRoles?.includes(Role.OrganizationAdmin) ||
+                                    currentUserRoles?.includes(Role.WorkspaceAdmin);
+                                if (!isAdmin && currentUserRoles) {
+                                    this.router.navigateByUrl(Constants.WELCOME_PAGE);
+                                    return;
+                                }
+                                this.outClose.emit(false);
+                            });
+                    });
             });
     }
 
