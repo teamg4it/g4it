af5584a7183af6c558ceb212404696553b355e3d
gauriairi <gauri.airi@soprasteria.com>
2026-01-27 14:18:53 +0530

    test git cred commit access check1

---
+++ services/backend/src/main/java/com/soprasteria/g4it/backend/config/JacksonXssConfig.java
@@ -0,0 +1,25 @@
+/*
+ * G4IT
+ * Copyright 2023 Sopra Steria
+ *
+ * This product includes software developed by
+ * French Ecological Ministery (https://gitlab-forge.din.developpement-durable.gouv.fr/pub/numeco/m4g/numecoeval)
+ */
+
+package com.soprasteria.g4it.backend.config;
+
+import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JacksonXssConfig {
+
+    @Bean
+    public Jackson2ObjectMapperBuilderCustomizer xssCustomizer() {
+        return builder -> builder.deserializerByType(
+                String.class,
+                new XssStringDeserializer()
+        );
+    }
+}
