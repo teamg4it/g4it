bd3a050a4f8ad2ff8ecdafd8a4eb427560be4d96
LankReD <maati.anouar1@gmail.com>
2026-01-28 01:01:39 +0100

    Retrait des modifications personnelles de l'application

--- services/frontend/src/app/layout/digital-services-footprint/digital-services-recommendations/digital-services-recommendations.component.ts
+++ services/frontend/src/app/layout/digital-services-footprint/digital-services-recommendations/digital-services-recommendations.component.ts
@@ -27,38 +27,31 @@ export class DigitalServicesRecommendationsComponent {
 
   recommendations = [
     { 
-      title: 'Titre de la recommendation', 
-      category: 'Terminaux', 
-      globalReduction: "00%", 
-      description: 'Déscription de la recommendation', 
-      selected: false 
-    },
-    { 
-      title: 'Titre de la recommendation', 
-      category: 'Réseaux', 
-      globalReduction: "00%", 
-      description: 'Déscription de la recommendation', 
-      selected: false 
-    },
-    { 
-      title: 'Titre de la recommendation', 
-      category: 'Infrastructures Privées', 
-      globalReduction: "00%", 
-      description: 'Déscription de la recommendation', 
-      selected: false 
-    },
-    { 
-      title: 'Titre de la recommendation', 
+      title: 'Utiliser une architecture adaptée à la quantité de ressources utilisées à la consommation du service', 
       category: 'Clouds Publics - IaaS', 
       globalReduction: "00%", 
-      description: 'Déscription de la recommendation', 
+      description: 'L’objectif est d’éviter une architecture surdimensionnée et de privilégier une architecture capable d\'ajuster dynamiquement la quantité de ressources utilisées en fonction de la demande du service, et passer à l’échelle. Cela contribue à optimiser l\'efficacité énergétique et à éviter le gaspillage de ressources inutiles.', 
       selected: false 
     },
     { 
-      title: 'Titre de la recommendation', 
-      category: 'Infrastructures Privées', 
+      title: 'S\'astreindre à un poids maximum et une limite de requêtes par écran', 
+      category: 'Réseaux', 
       globalReduction: "00%", 
-      description: 'Déscription de la recommendation', 
+      description: 'Réduire ou limiter les données téléchargées.', 
+      selected: false 
+    },
+    { 
+      title: 'Utiliser un hébergement dont le PUE (Power Usage Effectiveness) est minimisé', 
+      category: 'Clouds Publics - IaaS', 
+      globalReduction: "00%", 
+      description: 'Il s’agit de connaître le PUE de son hébergement et favoriser la réduction de la consommation d’énergie nécessaire au bon fonctionnement et au refroidissement des serveurs nécessaires à l’hébergement.', 
+      selected: false 
+    },
+    { 
+      title: 'Utiliser un hébergement dont la localisation géographique est cohérente avec ses activités et qui minimise son empreinte environnementale ', 
+      category: 'Clouds Publics - IaaS', 
+      globalReduction: "00%", 
+      description: 'L’objectif est d’abord de privilégier un hébergement dans le pays où l’intensité carbone est peu élevée, et secondairement, dans une région où se situent la majorité des clients, afin de réduire la distance parcourue par les données et donc réduire l’infrastructure réseau mobilisée et son empreinte environnementale.', 
       selected: false 
     },
   ];
