37c8e95208933f22fc8277a222ec26a0b8074515
TUSHAR <tushar.tushar2@soprasteria.com>
2026-01-30 17:46:58 +0530

    fix(1923): fix for space

--- services/frontend/src/app/layout/inventories-footprint/inventories-footprint.component.ts
+++ services/frontend/src/app/layout/inventories-footprint/inventories-footprint.component.ts
@@ -217,7 +217,6 @@ export class InventoriesFootprintComponent implements OnInit {
             this.allUnmodifiedFilters[field] = [
                 Constants.ALL,
                 ...uniqueFilterSet[field]
-
                     .map((item: any) => {
                         const value = typeof item === "string" ? item?.trim() : item;
                         return value?.length ? value : Constants.EMPTY;
