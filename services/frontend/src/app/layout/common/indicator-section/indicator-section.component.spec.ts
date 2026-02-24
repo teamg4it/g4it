67fa1a0afac1a6d59ccfabe75ca4079705db9b4d
TUSHAR <tushar.tushar2@soprasteria.com>
2026-02-24 17:32:56 +0530

    feat(1762): test case fix

--- services/frontend/src/app/layout/common/indicator-section/indicator-section.component.spec.ts
+++ services/frontend/src/app/layout/common/indicator-section/indicator-section.component.spec.ts
@@ -1,23 +1,23 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { TranslateModule } from "@ngx-translate/core";
 
-import { IndicatorSectionComponent } from './indicator-section.component';
+import { IndicatorSectionComponent } from "./indicator-section.component";
 
-describe('IndicatorSectionComponent', () => {
-  let component: IndicatorSectionComponent;
-  let fixture: ComponentFixture<IndicatorSectionComponent>;
+describe("IndicatorSectionComponent", () => {
+    let component: IndicatorSectionComponent;
+    let fixture: ComponentFixture<IndicatorSectionComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [IndicatorSectionComponent]
-    })
-    .compileComponents();
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            imports: [IndicatorSectionComponent, TranslateModule.forRoot()],
+        }).compileComponents();
 
-    fixture = TestBed.createComponent(IndicatorSectionComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+        fixture = TestBed.createComponent(IndicatorSectionComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it("should create", () => {
+        expect(component).toBeTruthy();
+    });
 });
