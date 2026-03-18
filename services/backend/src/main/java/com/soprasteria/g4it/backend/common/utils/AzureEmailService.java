c7733ab99a3bdff375af8551a9e58815573d53ad
gauriairi <gauri.airi@soprasteria.com>
2026-03-18 12:07:51 +0530

    G4IT_BLM-1912: email service codebase updated

--- services/backend/src/main/java/com/soprasteria/g4it/backend/common/utils/AzureEmailService.java
+++ services/backend/src/main/java/com/soprasteria/g4it/backend/common/utils/AzureEmailService.java
@@ -11,10 +11,17 @@ package com.soprasteria.g4it.backend.common.utils;
 import com.azure.communication.email.EmailAsyncClient;
 import com.azure.communication.email.EmailClientBuilder;
 import com.azure.communication.email.models.EmailMessage;
+import com.azure.communication.email.models.EmailSendResult;
+import com.azure.core.util.polling.AsyncPollResponse;
 import com.azure.core.util.polling.LongRunningOperationStatus;
+import com.azure.core.util.polling.PollerFlux;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import java.time.Duration;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 
 @Service
@@ -22,30 +29,44 @@ import org.springframework.stereotype.Service;
 public class AzureEmailService {
 
     private final EmailAsyncClient emailClient;
+    private final String senderEmail;
 
-    public AzureEmailService(@Value("${azure.communication.connection-string}") String connectionString) {
+    public AzureEmailService(@Value("${azure.communication.connection-string}") String connectionString,
+                             @Value("${azure.communication.sender-email}") String senderEmail) {
         this.emailClient = new EmailClientBuilder()
                 .connectionString(connectionString)
                 .buildAsyncClient();
+        this.senderEmail= senderEmail;
     }
 
     public void sendEmail(String recipient, String subject, String body) {
         EmailMessage emailMessage = new EmailMessage()
-                .setSenderAddress("DoNotReply@saas-g4it.com")
+                .setSenderAddress(senderEmail)
                 .setToRecipients(recipient)
                 .setSubject(subject)
                 .setBodyPlainText(body)
                 .setBodyHtml(body);
-        emailClient.beginSend(emailMessage).subscribe(
-                response -> {
-                    if (response.getStatus() == LongRunningOperationStatus.SUCCESSFULLY_COMPLETED) {
-                        log.info("Successfully sent the email operation id: {}", response.getValue().getId());
-                    }
-                    else {
-                        // The operation ID can be retrieved as soon as the first response is received from the PollerFlux.
-                        log.info("Email send status: {} , operation id: {}", response.getStatus(), response.getValue().getId());
-                    }
-                },error -> log.error("Error occurred while sending email: {}", error.getMessage())
-        );
+
+        PollerFlux<EmailSendResult, EmailSendResult> poller = emailClient.beginSend(emailMessage);
+        try (ExecutorService executorService = Executors.newSingleThreadExecutor()) {
+            executorService.submit(() -> poller.subscribe(
+                    this::handleAsyncPollerResponse,
+                    error -> log.error("Error occurred while sending email: {}", error.getMessage())
+            ));
+            try {
+                Thread.sleep(Duration.ofSeconds(30));
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                log.error("Thread was interrupted while waiting for email send operation.", e);
+            }
+        }
+    }
+
+    private void handleAsyncPollerResponse(AsyncPollResponse<EmailSendResult, EmailSendResult> response) {
+        if (response.getStatus() == LongRunningOperationStatus.SUCCESSFULLY_COMPLETED) {
+            log.info("Successfully sent the email operation id: {}", response.getValue().getId());
+        } else {
+            log.info("Email send status: {} , operation id: {}", response.getStatus(), response.getValue().getId());
+        }
     }
 }
