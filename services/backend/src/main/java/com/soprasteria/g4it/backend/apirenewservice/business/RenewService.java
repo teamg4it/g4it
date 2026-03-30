0146d6c312cd205745736a49cb9868a4690ec7c9
gauriairi <gauri.airi@soprasteria.com>
2026-03-30 17:05:42 +0530

    G4IT_BLM-1912: added renew service

--- services/backend/src/main/java/com/soprasteria/g4it/backend/apirenewservice/business/RenewService.java
+++ services/backend/src/main/java/com/soprasteria/g4it/backend/apirenewservice/business/RenewService.java
@@ -9,7 +9,9 @@
 package com.soprasteria.g4it.backend.apirenewservice.business;
 
 import com.soprasteria.g4it.backend.apidigitalservice.modeldb.DigitalService;
+import com.soprasteria.g4it.backend.apidigitalservice.modeldb.DigitalServiceVersion;
 import com.soprasteria.g4it.backend.apidigitalservice.repository.DigitalServiceRepository;
+import com.soprasteria.g4it.backend.apidigitalservice.repository.DigitalServiceVersionRepository;
 import com.soprasteria.g4it.backend.apiinventory.modeldb.Inventory;
 import com.soprasteria.g4it.backend.apiinventory.repository.InventoryRepository;
 import com.soprasteria.g4it.backend.apiuser.modeldb.Workspace;
@@ -34,16 +36,18 @@ public class RenewService {
     private final InventoryRepository inventoryRepository;
     private final DigitalServiceRepository digitalServiceRepository;
     private final WorkspaceRepository workspaceRepository;
+    private final DigitalServiceVersionRepository digitalServiceVersionRepository;
 
     @Value("${g4it.data.retention.day}")
     private Integer dataRetentiondDay;
 
     public RenewService(InventoryRepository inventoryRepository,
                         DigitalServiceRepository digitalServiceRepository,
-                        WorkspaceRepository workspaceRepository) {
+                        WorkspaceRepository workspaceRepository, DigitalServiceVersionRepository digitalServiceVersionRepository) {
         this.inventoryRepository = inventoryRepository;
         this.digitalServiceRepository = digitalServiceRepository;
         this.workspaceRepository = workspaceRepository;
+        this.digitalServiceVersionRepository = digitalServiceVersionRepository;
     }
 
     public RenewRest getRenewDetailsInventory(Long workspace, Long inventoryId) {
@@ -63,39 +67,43 @@ public class RenewService {
                 .expiryDate(expirationDate).retentionDays(retentionDay).build();
     }
 
-    public RenewRest getRenewDetailsDigitalService(Long workspace, String digitalServiceUid) {
+    public RenewRest getRenewDetailsDigitalService(Long workspace, String digitalServiceVersionUid) {
         Workspace workspaceEntity = workspaceRepository.findById(workspace).orElseThrow(() ->
                 new G4itRestException("400", String.format(ErrorConstants.WORKSPACE_NOT_FOUND, workspace)));
         final LocalDateTime now = LocalDateTime.now();
         final Integer retentionDay = Optional.ofNullable(workspaceEntity.getDataRetentionDay())
                 .orElse(Optional.ofNullable(workspaceEntity.getOrganization().getDataRetentionDay())
                         .orElse(dataRetentiondDay));
-        DigitalService digitalService = digitalServiceRepository.findByWorkspaceAndUid(workspaceEntity, digitalServiceUid).orElseThrow(() ->
-                new G4itRestException("400", String.format(ErrorConstants.DIGITAL_SERVICE_NOT_FOUND, digitalServiceUid)));
-        long daysSinceLastUpdate = java.time.Duration.between(
-                digitalService.getLastUpdateDate(), now
-        ).toDays();
-        String expirationDate = now.plusDays(retentionDay - daysSinceLastUpdate).toLocalDate().toString();
-        return RenewRest.builder().serviceName(digitalService.getName()).serviceId(digitalService.getUid())
-                .expiryDate(expirationDate).retentionDays(retentionDay).build();
+        Optional<DigitalServiceVersion> digitalServiceVersion = digitalServiceVersionRepository.findById(digitalServiceVersionUid);
+        if(digitalServiceVersion.isPresent()) {
+            DigitalService digitalService = digitalServiceVersion.get().getDigitalService();
+            long daysSinceLastUpdate = java.time.Duration.between(
+                    digitalService.getLastUpdateDate(), now
+            ).toDays();
+            String expirationDate = now.plusDays(retentionDay - daysSinceLastUpdate).toLocalDate().toString();
+            return RenewRest.builder().serviceName(digitalService.getName()).serviceId(digitalService.getUid())
+                    .expiryDate(expirationDate).retentionDays(retentionDay).build();
+        }else{
+            throw new G4itRestException("400", String.format(ErrorConstants.DIGITAL_SERVICE_NOT_FOUND, digitalServiceVersionUid));
+        }
     }
 
-    public RenewResponseRest renewDigitalService(Long workspace, String digitalServiceUid,RenewUpdateRest renewUpdateRest) {
-        if (renewUpdateRest.getAction()!=null && renewUpdateRest.getAction().equals("renew")) {
+    public RenewResponseRest renewDigitalService(Long workspace, RenewUpdateRest renewUpdateRest) {
+        if (renewUpdateRest.getAction() != null && renewUpdateRest.getAction().equals("renew")) {
             Workspace workspaceEntity = workspaceRepository.findById(workspace).orElseThrow(() ->
                     new G4itRestException("400", String.format(ErrorConstants.WORKSPACE_NOT_FOUND, workspace)));
-            DigitalService digitalService = digitalServiceRepository.findByWorkspaceAndUid(workspaceEntity, digitalServiceUid).orElseThrow(() ->
-                    new G4itRestException("400", String.format(ErrorConstants.DIGITAL_SERVICE_NOT_FOUND, digitalServiceUid)));
+            DigitalService digitalService = digitalServiceRepository.findByWorkspaceAndUid(workspaceEntity, renewUpdateRest.getServiceId()).orElseThrow(() ->
+                    new G4itRestException("400", String.format(ErrorConstants.DIGITAL_SERVICE_NOT_FOUND, renewUpdateRest.getServiceId())));
             digitalService.setLastUpdateDate(LocalDateTime.now());
             digitalServiceRepository.save(digitalService);
             return RenewResponseRest.builder().serviceId(digitalService.getUid()).isRenewed(true).responseMessage(Constants.RENEWAL_SUCCESS_MESSAGE).build();
-        }else{
+        } else {
             throw new G4itRestException("400", ErrorConstants.INVALID_RENEW_ACTION);
         }
     }
 
     public RenewResponseRest renewInventoryService(Long workspace, Long inventoryId, RenewUpdateRest renewUpdateRest) {
-        if (renewUpdateRest.getAction()!=null && renewUpdateRest.getAction().equals("renew")) {
+        if (renewUpdateRest.getAction() != null && renewUpdateRest.getAction().equals("renew")) {
             workspaceRepository.findById(workspace).orElseThrow(() ->
                     new G4itRestException("400", String.format(ErrorConstants.WORKSPACE_NOT_FOUND, workspace)));
             Inventory inventory = inventoryRepository.findById(inventoryId).orElseThrow(() ->
@@ -103,7 +111,7 @@ public class RenewService {
             inventory.setLastUpdateDate(LocalDateTime.now());
             inventoryRepository.save(inventory);
             return RenewResponseRest.builder().serviceId(String.valueOf(inventory.getId())).isRenewed(true).responseMessage(Constants.RENEWAL_SUCCESS_MESSAGE).build();
-        }else{
+        } else {
             throw new G4itRestException("400", ErrorConstants.INVALID_RENEW_ACTION);
         }
     }
