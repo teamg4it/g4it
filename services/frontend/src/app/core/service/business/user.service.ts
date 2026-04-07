ad8319a2bb5ccfd3e42ee1a6ad54120d29a5ef51
TUSHAR <tushar.tushar2@soprasteria.com>
2026-04-07 11:55:46 +0530

    feat(1991): update organization workspace name

--- services/frontend/src/app/core/service/business/user.service.ts
+++ services/frontend/src/app/core/service/business/user.service.ts
@@ -394,17 +394,17 @@ export class UserService {
         return `mailto:${Constants.RECIPIENT_MAIL}?subject=${subject}`;
     }
 
-    composeEcoMindAccessEmail(userEmail: string): string {
+    composeEcoMindAccessEmail(orgName: string, workspaceName: string): string {
         const subject = encodeURIComponent("EcoMindAI access");
-
+        const orgWorkspaceName = `[${workspaceName} (${orgName})]`;
         const body = encodeURIComponent(
             `Hello,
-            I request the access to the module EcoMindAI Module of G4IT on the workspace named [DEMO (Sopra-Steria-Group)]
+            I request the access to the module EcoMindAI Module of G4IT on the workspace named ${orgWorkspaceName}
             What I expect of the tool is :
             The context of my project is :
             ---------------------------------------------------------------------
-            Bonjour, 
-            J'aimerais accéder au module EcoMindAI de G4IT sur l'espace de travail nommé [DEMO (Sopra-Steria-Group)]
+            Bonjour,
+            J'aimerais accéder au module EcoMindAI de G4IT sur l'espace de travail nommé ${orgWorkspaceName}
             Voici ce que je j'aimerais faire avec l'outil :
             Voici le contexte de mon projet : `,
         );
