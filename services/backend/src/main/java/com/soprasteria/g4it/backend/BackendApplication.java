e44c12c2f27c593ac96da49005a147f8ba0311fd
gauriairi <gauri.airi@soprasteria.com>
2026-04-13 12:36:30 +0530

    G4IT_BLM-1976: analysis for migration spring boot 4

--- services/backend/src/main/java/com/soprasteria/g4it/backend/BackendApplication.java
+++ services/backend/src/main/java/com/soprasteria/g4it/backend/BackendApplication.java
@@ -10,12 +10,14 @@ package com.soprasteria.g4it.backend;
 import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 import java.util.TimeZone;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 @EnableCaching
 @EnableScheduling
 public class BackendApplication {
