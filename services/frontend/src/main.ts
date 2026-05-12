7d30ee9baf7b91954549a94e5aad067b6d10305c
TUSHAR <tushar.tushar2@soprasteria.com>
2026-05-12 23:59:58 +0530

    feat(1897): angular 21 migration

--- services/frontend/src/main.ts
+++ services/frontend/src/main.ts
@@ -5,7 +5,7 @@
  * This product includes software developed by
  * French Ecological Ministery (https://gitlab-forge.din.developpement-durable.gouv.fr/pub/numeco/m4g/numecoeval)
  */
-import { enableProdMode } from "@angular/core";
+import { enableProdMode, provideZoneChangeDetection } from "@angular/core";
 import { bootstrapApplication } from "@angular/platform-browser";
 import { AppComponent } from "./app/app.component";
 import { appConfig } from "./app/app.config";
@@ -15,4 +15,7 @@ if (environment.production) {
     enableProdMode();
 }
 
-bootstrapApplication(AppComponent, appConfig).catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+    ...appConfig,
+    providers: [provideZoneChangeDetection(), ...appConfig.providers],
+}).catch((err) => console.error(err));
