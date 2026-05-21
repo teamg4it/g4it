94c6a9a1a95a93cacf834f9e17e25a1899e921df
gauriairi <gauri.airi@soprasteria.com>
2026-05-21 12:49:28 +0530

    G4IT_BLM-2181: performmance issue fix

--- services/backend/src/main/java/com/soprasteria/g4it/backend/apireferential/repository/ItemImpactRepository.java
+++ services/backend/src/main/java/com/soprasteria/g4it/backend/apireferential/repository/ItemImpactRepository.java
@@ -73,12 +73,12 @@ public interface ItemImpactRepository extends JpaRepository<ItemImpact, Long> {
 
     @Query("""
             SELECT cf FROM #{#entityName} cf WHERE
-            ((?1 IS NULL) OR (?1 IS NOT NULL AND cf.criterion = ?1)) AND
-            ((?2 IS NULL) OR (?2 IS NOT NULL AND cf.lifecycleStep = ?2)) AND
-            ((?3 IS NULL) OR (?3 IS NOT NULL AND cf.name = ?3)) AND
-            ((?4 IS NULL) OR (?4 IS NOT NULL AND cf.category = ?4)) AND
-            ((?5 IS NULL) OR (?5 IS NOT NULL AND cf.location = ?5)) AND
-            ((?6 IS NULL) OR (?6 IS NOT NULL AND cf.workspaceId = ?6))
+            cf.criterion = ?1 AND
+            cf.lifecycleStep = ?2 AND
+            cf.name = ?3 AND
+            cf.category = ?4 AND
+            cf.location = ?5 AND
+            cf.workspaceId = ?6
             """)
     List<ItemImpact> findByCriterionAndLifecycleStepAndNameAndCategoryAndLocationAndWorkspaceId(final String criterion,
                                                                                                  final String lifecycleStep,
@@ -93,13 +93,13 @@ public interface ItemImpactRepository extends JpaRepository<ItemImpact, Long> {
 
     @Query("""
             SELECT cf FROM #{#entityName} cf WHERE
-            ((?1 IS NULL) OR (?1 IS NOT NULL AND cf.criterion = ?1)) AND
-            ((?2 IS NULL) OR (?2 IS NOT NULL AND cf.lifecycleStep = ?2)) AND
-            ((?3 IS NULL) OR (?3 IS NOT NULL AND cf.name = ?3)) AND
-            ((?4 IS NULL) OR (?4 IS NOT NULL AND cf.category = ?4)) AND
-            ((?5 IS NULL) OR (?5 IS NOT NULL AND cf.location = ?5)) AND
-            ((?6 IS NULL) OR (?6 IS NOT NULL AND cf.organization = ?6)) AND
-            ((?7 IS NULL) OR (?7 IS NOT NULL AND cf.workspaceId = ?7))
+            cf.criterion = ?1 AND
+            cf.lifecycleStep = ?2 AND
+            cf.name = ?3 AND
+            cf.category = ?4 AND
+            cf.location = ?5 AND
+            cf.organization = ?6 AND
+            cf.workspaceId = ?7
             """)
     List<ItemImpact> findByCriterionAndLifecycleStepAndNameAndCategoryAndLocationAndOrganizationAndWorkspaceId(final String criterion,
                                                                                                  final String lifecycleStep,
