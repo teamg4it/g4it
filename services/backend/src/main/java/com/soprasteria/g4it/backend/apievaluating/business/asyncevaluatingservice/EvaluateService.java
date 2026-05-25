72927a889f9151cc9f119f3d02899572bd32437e
gauriairi <gauri.airi@soprasteria.com>
2026-05-25 17:56:58 +0530

    G4IT_BLM-2181: memory fix

--- services/backend/src/main/java/com/soprasteria/g4it/backend/apievaluating/business/asyncevaluatingservice/EvaluateService.java
+++ services/backend/src/main/java/com/soprasteria/g4it/backend/apievaluating/business/asyncevaluatingservice/EvaluateService.java
@@ -388,7 +388,7 @@ public class EvaluateService {
                     outApplicationSize += saveResult2.savedApplicationCount();
 
                     physicalSaveCounter++;
-                    if (physicalSaveCounter >= 10) {
+                    if (physicalSaveCounter >= 5) {
                         outPhysicalEquipmentSize += saveService.saveOutPhysicalEquipments(
                                 aggregationPhysicalEquipments, taskId, refShortcutBO);
                         aggregationPhysicalEquipments = new HashMap<>(INITIAL_MAP_CAPACITY);
@@ -572,7 +572,7 @@ public class EvaluateService {
 
                 virtualSaveCounter++;
 
-                if (virtualSaveCounter >= 10) {
+                if (virtualSaveCounter >= 5) {
                     savedVirtualCount += saveService.saveOutVirtualEquipments(
                             aggregationVirtualEquipments, evaluateReportBO.getTaskId(), refShortcutBO);
                     aggregationVirtualEquipments.clear();
@@ -654,7 +654,7 @@ public class EvaluateService {
 
             applicationSaveCounter++;
 
-            if (applicationSaveCounter >= 10) {
+            if (applicationSaveCounter >= 5) {
                 savedApplicationCount += saveService.saveOutApplications(
                         aggregationApplications,
                         evaluateReportBO.getTaskId(),
