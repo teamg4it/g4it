0a8efa26deb4b3e191bb907a0c197355f2fa7c57
gauriairi <gauri.airi@soprasteria.com>
2026-05-29 18:20:30 +0530

    G4IT_BLM-2181: parantadded

--- services/backend/src/main/java/com/soprasteria/g4it/backend/apifiles/business/FileSystemService.java
+++ services/backend/src/main/java/com/soprasteria/g4it/backend/apifiles/business/FileSystemService.java
@@ -207,7 +207,10 @@ public class FileSystemService {
         final StringBuilder tempPath = Boolean.TRUE.equals(isInventory) ? new StringBuilder(localWorkingFolder).append(File.separator).append("input").append(File.separator).append("inventory").append(File.separator).append(UUID.randomUUID())
                 : new StringBuilder(localWorkingFolder).append(File.separator).append("input").append(File.separator).append("digital-service").append(File.separator).append(UUID.randomUUID());
         File outputFile = new File(tempPath.toString());
-
+        File parent = outputFile.getParentFile();
+        if (!parent.exists()) {
+            parent.mkdirs();
+        }
         // Detect file type by extension
         String extension = StringUtils.getFilenameExtension(file.getOriginalFilename());
 
