45c231913b6b113e78f5502936ec1584aa1b0d8f
gauriairi <gauri.airi@soprasteria.com>
2026-06-16 16:53:45 +0530

    G4IT_BLM-2173: migration to Spring boot v4

--- services/backend/src/test/java/com/soprasteria/g4it/backend/functionaltest/FunctionalTests.java
+++ services/backend/src/test/java/com/soprasteria/g4it/backend/functionaltest/FunctionalTests.java
@@ -48,9 +48,10 @@ import java.util.Map;
 
 @SpringBootTest(
         properties = {
-                "spring.cloud.azure.enabled=false",
+                "spring.cloud.azure.storage.blob.enabled=false",
                 "spring.liquibase.enabled=false",
-                "spring.jpa.hibernate.ddl-auto=create-drop"
+                "spring.jpa.hibernate.ddl-auto=create-drop",
+                "spring.cloud.azure.keyvault.secret.enabled=false"
         }
 )
 @ActiveProfiles({"local", "test"})
@@ -88,7 +89,7 @@ class FunctionalTests {
     CheckApplicationRepository checkApplicationRepository;
 
 
-    @Mock
+    @MockitoBean
     BoaviztapiService boaviztapiService;
 
     @Test
