ac0262e264df14ce0583495c79fa17d2f934ffd6
TUSHAR <tushar.tushar2@soprasteria.com>
2026-07-07 15:14:38 +0530

    feat(2158): fix tests

--- services/frontend/src/app/layout/common/inverse-axis-button/inverse-axis-button.component.spec.ts
+++ services/frontend/src/app/layout/common/inverse-axis-button/inverse-axis-button.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from "@angular/core/testing";
-
+import { TranslateLoader, TranslateModule } from "@ngx-translate/core";
+import { of } from "rxjs";
 import { InverseAxisButtonComponent } from "./inverse-axis-button.component";
 
 describe("InverseAxisButtonComponent", () => {
@@ -8,7 +9,17 @@ describe("InverseAxisButtonComponent", () => {
 
     beforeEach(async () => {
         await TestBed.configureTestingModule({
-            imports: [InverseAxisButtonComponent],
+            imports: [
+                InverseAxisButtonComponent,
+                TranslateModule.forRoot({
+                    loader: {
+                        provide: TranslateLoader,
+                        useValue: {
+                            getTranslation: () => of({}),
+                        },
+                    },
+                }),
+            ],
         }).compileComponents();
 
         fixture = TestBed.createComponent(InverseAxisButtonComponent);
@@ -18,5 +29,57 @@ describe("InverseAxisButtonComponent", () => {
 
     it("should create", () => {
         expect(component).toBeTruthy();
+    });
+
+    it("should initialize with isInverted as false", () => {
+        expect(component.isInverted()).toBe(false);
+    });
+
+    it("should toggle isInverted to true on first call to toggleAxis", () => {
+        component.toggleAxis();
+        expect(component.isInverted()).toBe(true);
+    });
+
+    it("should toggle isInverted back to false on second call to toggleAxis", () => {
+        component.toggleAxis(); // First toggle: false -> true
+        component.toggleAxis(); // Second toggle: true -> false
+        expect(component.isInverted()).toBe(false);
+    });
+
+    it("should emit true when toggleAxis is called from initial state", () => {
+        let emittedValue: boolean | undefined;
+        component.inverseAxisChange.subscribe((value: boolean) => {
+            emittedValue = value;
+        });
+
+        component.toggleAxis();
+
+        expect(emittedValue).toBe(true);
+    });
+
+    it("should emit false when toggleAxis is called from inverted state", () => {
+        component.isInverted.set(true); // Set to inverted state
+
+        let emittedValue: boolean | undefined;
+        component.inverseAxisChange.subscribe((value: boolean) => {
+            emittedValue = value;
+        });
+
+        component.toggleAxis();
+
+        expect(emittedValue).toBe(false);
+    });
+
+    it("should emit values on multiple toggles", () => {
+        const emittedValues: boolean[] = [];
+        component.inverseAxisChange.subscribe((value: boolean) => {
+            emittedValues.push(value);
+        });
+
+        component.toggleAxis(); // Emit true
+        component.toggleAxis(); // Emit false
+        component.toggleAxis(); // Emit true again
+
+        expect(emittedValues).toEqual([true, false, true]);
     });
 });
